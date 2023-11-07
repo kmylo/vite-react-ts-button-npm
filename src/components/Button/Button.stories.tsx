c7ef9bb265fa8da1c578f99f263eb7b4ee6199db
kmylo <kmylo6@gmail.com>
2023-11-07 02:49:23 +0000

    Update 3 files

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,47 @@
-import { Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
+
 import { Button } from "./Button";
 
-export default {
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "in2/Button",
   component: Button,
-  title: "Button",
-} as Meta;
-export const Primary = () => <Button>Primary Button</Button>;
-// export const Secondary = () => (
-//   <Button variant="secondary">Secondary Button</Button>
-// );
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+    children: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    children: "Button",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    children: "Button",
+  },
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,34 @@
 import { ReactNode, useCallback } from "react";
-import "../../index.css";
+// import "../../index.css";
+import "./Button.css";
 
 interface Props {
   children?: ReactNode;
-  props?: any;
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: "small" | "medium" | "large";
 }
 
-export const Button = ({ children, ...props }: Props) => {
+export const Button = ({
+  children,
+  primary = false,
+  size = "medium",
+  ...props
+}: Props) => {
+  const mode = primary ? "in2-button--primary" : "in2-button--secondary";
+
   const onClick = useCallback(() => {
     console.log("Vite + React + TypeScript + Tailwind = ❤️");
   }, []);
 
+  const defaultVar = `bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded`;
+
   return (
     <button
+      type="button"
       onClick={onClick}
-      className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded`}
+      className={["in2-button", `in2-button--${size}`, mode].join(" ")}
+      {...props}
     >
       {children ? children : "Vite Btn npm"}
     </button>
